8e3115331fe86c302b2439b5cdf901de053a767e
Sebastian Bara <sebastianbara24@gmail.com>
2026-06-08 16:30:01 +0200

    game structure added

--- js/game.js
+++ js/game.js
@@ -1,7 +1,12 @@
 let canvas;
-let character = new Image();
+let ctx;
+let character = new MovableObject(20, 20, 'img/wizards/PNG/2_WIZARD/Wizard_02__IDLE_000.png');
 
 function initializeGame() {
     canvas = document.getElementById('gameCanvas');
-    character.src = '../img/wizards/PNG/2_WIZARD/WIZARD_02_IDLE_000.png';
+    ctx = canvas.getContext('2d');
+
+    ctx.drawImage(character.img, character.x, character.y, 50, 150);
+
+    console.log('My Character is: ', character);
 }
