a00528d75d6a46e9cf6daf4be35bea5c894d7a19
Sebastian Bara <sebastianbara24@gmail.com>
2026-06-08 15:55:57 +0200

    first commit

---
+++ img/enemies/_TXT/readme.txt
@@ -0,0 +1,2 @@
+ font -  firewood
+https://www.dafont.com/fire-wood.font?l[]=10&l[]=1&text=ENT
---
+++ js/game.js
@@ -0,0 +1,7 @@
+let canvas;
+let character = new Image();
+
+function initializeGame() {
+    canvas = document.getElementById('gameCanvas');
+    character.src = '../img/wizards/PNG/2_WIZARD/WIZARD_02_IDLE_000.png';
+}
